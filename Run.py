ceb88710632720ce8efcd2e0b58675aac7639399
Hitham2496 <61147737+Hitham2496@users.noreply.github.com>
2021-07-15 15:11:45 +0100

    Update Run.py for scale variations
    Update runcard to provide argument for scale variations to be produced.

--- Run.py
+++ Run.py
@@ -21,4 +21,4 @@ from matplotlib.lines import Line2D # yet more plotting
 #dy_corr = [r"$0\leq \Delta y \leq 1$", r"$1\leq \Delta y \leq 2$",r"$2\leq \Delta y \leq 3$",r"$3\leq \Delta y \leq 4$",r"$4\leq \Delta y \leq 5$",
 #        r"$5\leq \Delta y \leq 6$",r"$6\leq \Delta y \leq 7$",r"$7\leq \Delta y \leq 8$"]
 
-plot_single([unpack("h_dy_jet12_2j.dat")], r"$\Delta y_{12}$", r"$d\sigma_{W+\geq 2j}/d \Delta y_{12}$", "h_dy_jet12_2j", (1.,0.2), (7e-4,80))
+plot_single([unpack("h_dy_jet12_2j.dat")], r"$\Delta y_{12}$", r"$d\sigma_{W+\geq 2j}/d \Delta y_{12}$", "h_dy_jet12_2j", (1.,0.2), (7e-4,80), [0])
