aad9d8818bda74cb11492dc34cc18eb5a067a1eb
Hitham2496 <61147737+Hitham2496@users.noreply.github.com>
2022-08-20 22:16:02 +0100

    Update Run.py to work with refactored Plotter.py

--- Run.py
+++ Run.py
@@ -4,13 +4,14 @@
 Plotting commands
 @author: hhassan
 """
-import Plotter
+from Plotter import plotter
 
 def main():
-
-    Plotter.plot_single(Plotter.unpack("h_dy_jet12_2j.dat"), "h_dy_jet12_2j", (1.,0.2), (7e-4,80), [0],
-                                       r"$\Delta y_{12}$", r"$d\sigma_{W+\geq 2j}/d \Delta y_{12}$",
-                                       r"$pp \rightarrow Wjj$ at $\sqrt{s}=$13~TeV\par anti-$k_\perp$ jets, $R=$~0.4, $k_\perp>$~60 GeV, $y_j<$~2.8")
+    LEGSTR = r'$pp\rightarrow Wjj at $\sqrt{s}=13$ TeV'+'\n'+r'anti-$k_\perp$ jets, $R=0.4$, $<k_{\perp} > 60$ GeV, $y_j<2.8$'
+    LEGPOS = 'lower left'
+    plot_routine = plotter(LEGPOS, LEGSTR)
+    plot_routine.plot_single(Plotter.unpack("h_dy_jet12_2j.dat"), r"$\Delta y_{12}$", r"$d\sigma_{W+\geq 2j}/d \Delta y_{12}$",
+                                            "h_dy_jet12_2j", (1.,0.2), (7e-4,80), [0]))
 
 if __name__ == '__main__':
   main()
